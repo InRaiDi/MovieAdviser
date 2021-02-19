eabbcd80ed5dabb8e50bb1f47826a56ef8e92e5b
Dinara <inraidi@gmail.com>
2021-02-19 18:11:13 -0500

    Front-End for Login page + controller

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -12,5 +12,8 @@ module.exports = function (app) {
    app.get('/registration', function(req,res){
       res.render('registration', {title:'Registration'});
      });
+     app.get('/login', function(req,res){
+      res.render('login', {title:'Log In'});
+     });
 
 };
