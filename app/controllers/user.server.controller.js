cef6a5e817450dc7d99a3119b0fea4ddb533c8b9
Dinara <inraidi@gmail.com>
2021-03-02 12:29:32 -0500

    Back-End for User registration

---
+++ app/controllers/user.server.controller.js
@@ -0,0 +1,17 @@
+const User = require('mongoose').model('User');
+
+//adding new user to DB
+exports.register = function( req, res, next){
+    const user = new User(req.body);
+    console.log(user);
+    student.save((err) =>{
+        if(err){
+        return next(err);
+    } 
+    else{
+        res.render('registration', {
+            title: 'Index',
+        });
+    }
+    });
+};
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,7 +1,7 @@
 //this function handles routing of requests
 module.exports = function (app) {
    var index = require('../controllers/index.server.controller');
-
+   var user = require('../controllers/user.server.controller');
    app.get('/', function(req,res){
     res.render('homepage', {title:'Movies Website'});
    });
@@ -12,8 +12,13 @@ module.exports = function (app) {
    app.get('/registration', function(req,res){
       res.render('registration', {title:'Registration'});
      });
-     app.get('/login', function(req,res){
+
+   app.post('/registration', user.register);
+
+   app.get('/login', function(req,res){
       res.render('login', {title:'Log In'});
      });
+
+
 
 };
