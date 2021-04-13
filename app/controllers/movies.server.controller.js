372b3ec8c9fece396bfe75b360a8b775d982aff9
Dinesh <dineshbalakrishnan12@gmail.com>
2021-04-13 11:10:44 -0400

    Added Recommended Section
    Movies: Recommended Movies page

--- app/controllers/movies.server.controller.js
+++ app/controllers/movies.server.controller.js
@@ -8,6 +8,8 @@ const UpcomingMovies = require('mongoose').model('Movie');
 
 const topRateMovie = require("../models/movies.server.model");
 const TopRateMovies = require('mongoose').model('Movie');
+const recommendedMovies = require("../models/movies.server.model");
+const RecommendedMovies = require('mongoose').model('Movie');
 
 exports.movieadd = function (req, res) {
     const ret = {};
@@ -20,8 +22,20 @@ exports.insert = function (req, res ) {
     const movies = new Movies(req.body);
     const upcomingMovies = new Movies(req.body);
     const topRateMovies = new Movies(req.body);
+    const recommendedMovies = new Movies(req.body);
 
     console.log(movies);
+    movie.create(req.body, function(err, retobj){
+        if(err){
+            ret.msg = err.message;
+            res.json({ret});
+        }
+        else{
+        
+        res.redirect("/admin/list");
+        }
+      }); 
+    console.log(recommendedMovies);
     movie.create(req.body, function(err, retobj){
         if(err){
             ret.msg = err.message;
@@ -98,7 +112,7 @@ exports.updt = function (req, res) {
     try{
       const url='https://api.themoviedb.org/3/movie/now_playing?api_key=6a879a78d6083b8f3ba308233e0de85b&language=en-US&page=1';
       const url2='https://api.themoviedb.org/3/movie/upcoming?api_key=6a879a78d6083b8f3ba308233e0de85b&language=en-US&page=1';
-      const url3='https://api.themoviedb.org/3/movie/top_rated?api_key=6a879a78d6083b8f3ba308233e0de85b&language=en-US&page=1';
+      const url3 = 'https://api.themoviedb.org/3/movie/top_rated?api_key=6a879a78d6083b8f3ba308233e0de85b&language=en-US&page=1';
       fetch(url)
       .then(res => res.json())
       .then(async(json) =>{
@@ -190,6 +204,37 @@ exports.updt = function (req, res) {
             res.redirect("/admin/list");
           }
         } 
+      }).catch(err => res.send(err));;
+
+      fetch(url, url2, url3)
+      .then(res => res.json())
+      .then(async(json) =>{
+        let data=json;
+        if(data.hasOwnProperty('results')){
+          if(data.results.length>0){
+
+            for(const obj of data.results){
+              const movie=await Movies.findOne({id:obj.id}); ///checking if it's already in DB
+
+              if(!movie){ ///if not found then insert a new movie
+
+                const newMovie=new Movies({ 
+                  id:obj.id,
+                  title:obj.original_title,
+                  overview:obj.overview,
+                  releaseDate:obj.release_date,
+                  voteAverage:obj.vote_average,
+                  voteCount:obj.vote_count,
+                  language:obj.original_language,
+                  posterPath:obj.poster_path,
+                  movieType: 'recommended'
+                })
+                await newMovie.save();
+              } 
+            }
+            res.redirect("/admin/list");
+          }
+        }
       }).catch(err => res.send(err));;
 
 
