c556b682b914098358b336e9823feb4b335c4b6c
Nekitamr <melyanov.nikita@gmail.com>
2021-04-13 20:34:35 -0400

    Actual number of movies in Admin panel

--- app/controllers/admin.server.controller.js
+++ app/controllers/admin.server.controller.js
@@ -1,8 +1,14 @@
 
 const movie = require("../models/movies.server.model");
 const adminPage = function(req, res){
+
+    movie.find({}, function (err, retobj) {
+        let ret = {};
+        ret.movies = retobj;
+    res.render('admin-panel/index', {title:'Admin Panel', userLogged: req.user, data:ret});
+    });
   
-    res.render('admin-panel/index', {title:'Admin Panel', userLogged: req.user});
+    //res.render('admin-panel/index', {title:'Admin Panel', userLogged: req.user});
 
 }
 
